87aa2a6393692af21d3fe79074d57da8ab2c6a4d
Tomas Petrovaj <tomas.petrovaj@gmail.com>
2026-09-04 16:44:45 +0200

    fixed bugs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as exec from '@actions/exec'
 
 function run() {
     core.notice("Hello from my custom JavaScript Action!")
