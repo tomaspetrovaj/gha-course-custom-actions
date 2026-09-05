78dd21d8a50dcfa33a3550fd2b10d97dfb7a573e
Tomas Petrovaj <tomas.petrovaj@gmail.com>
2026-09-05 10:37:44 +0200

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,16 @@ import * as github from '@actions/github'
 import * as exec from '@actions/exec'
 
 function run() {
+    // 1.) Get inputs
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+    
+    // 2.) Authentication & Files upload
+    const s3Uri = `s3://${bucket}`
+    
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
     core.notice("Hello from my custom JavaScript Action!")
 }
 
