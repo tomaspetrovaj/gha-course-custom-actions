8d94aa079441c50dc13f3cd9c59ea9b34a4ef897
Tomas Petrovaj <tomas.petrovaj@gmail.com>
2026-09-05 11:03:48 +0200

    added output to custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,9 @@ function run() {
     
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
-    core.notice("Hello from my custom JavaScript Action!")
+    const websiteUrl = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    // set output
+    core.setOutput("website-url", websiteUrl)
 }
 
 run();
